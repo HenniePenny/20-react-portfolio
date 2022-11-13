6fdffe77ba7e02b417a1ab9a71a64cab888cbe57
HenniePenny <h.kressner@hotmail.de>
2022-11-13 22:24:47 +0100

    change className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
   return (
-    <div className="container">
+    <div className="container-fluid">
       <FontAwesomeIcon icon="fa-brands fa-github" />
       <FontAwesomeIcon icon="fa-brands fa-linkedin" />
     </div>
