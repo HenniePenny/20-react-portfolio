90c37dbde651f168d0962b9aa0a16d89c6e147e2
HenniePenny <h.kressner@hotmail.de>
2022-11-13 15:34:05 +0100

    add portfolio data and map function

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,10 +1,52 @@
 import React from "react";
+import Project from "../Project";
+import StudyBuddy from "../../assets/StudyBudy_screenshot.png";
+import FieldPotato from "../../assets/FieldPotato_screenshot.png";
+import Jate from "../../assets/JATE_screenshot.JPG";
+import NoteTaker from "../../assets/ExpressJSNOteTaker_screenshot.JPG";
+import WeatherDash from "../../assets/Weather-dashboard.JPG";
+
+const projectData = [
+  {
+    image: StudyBuddy,
+    title: "Study Buddy - Creating study groups made easy",
+    linkDeployed:
+      "https://github.com/HenniePenny/Study-Buddy/deployments/activity_log?environment=study-buddy-10178",
+    linkGH: "https://github.com/HenniePenny/Study-Buddy",
+  },
+  {
+    image: FieldPotato,
+    title: "Countless ways to move you from couch to field",
+    linkDeployed: "linkD.com",
+    linkGH: "linkGH.com",
+  },
+  {
+    image: Jate,
+    title: "PWA text editor - JATE",
+    linkDeployed: "https://serene-brushlands-11314.herokuapp.com/",
+    linkGH: "https://github.com/HenniePenny/19-PWA-text-editor",
+  },
+  {
+    image: NoteTaker,
+    title: "Express.js Note Taker",
+    linkDeployed: "https://limitless-island-12230.herokuapp.com/",
+    linkGH: "https://github.com/HenniePenny/11-Express.js-Note-Taker",
+  },
+  {
+    image: WeatherDash,
+    title: "Weather Dashboard",
+    linkDeployed: "https://limitless-island-12230.herokuapp.com/",
+    linkGH: "https://github.com/HenniePenny/11-Express.js-Note-Taker",
+  },
+];
 
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <p>My portfolio page here.</p>
+      {projectData.map((project) => (
+        <Project key={project.title} projectData={project} />
+      ))}
     </div>
   );
 }
