f1589410a92d29a631391a2b8ddaf918204c44f1
HenniePenny <h.kressner@hotmail.de>
2022-11-13 21:27:21 +0100

    add footer with icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <FontAwesomeIcon icon="fa-brands fa-github" />
+      <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+    </div>
+  );
 }
