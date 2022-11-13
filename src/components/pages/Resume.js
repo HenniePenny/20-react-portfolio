ac0726abbea07ef11a7c74970ca4f15cf8164be1
HenniePenny <h.kressner@hotmail.de>
2022-11-13 22:15:45 +0100

    added message and link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,9 +2,20 @@ import React from "react";
 
 export default function Resume() {
   return (
-    <div className="container">
-      <h1>Resume</h1>
-      <p>Click here to download my resume.</p>
+    <div className="container-fluid">
+      <h2>Resume</h2>
+      <p>
+        Click{" "}
+        <a
+          href="https://drive.google.com/file/d/1cU2SPT9yQNIyDE1Xrf7xpghfwuHW0S93/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer noopener"
+          download
+        >
+          here
+        </a>{" "}
+        to download my resume.
+      </p>
     </div>
   );
 }
