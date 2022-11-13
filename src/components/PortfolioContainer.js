9b74a1e1440b2c503f1f1370da0c1a4e6b3fee7b
HenniePenny <h.kressner@hotmail.de>
2022-11-13 21:27:48 +0100

    adjust PortfolioContainer for Footer and Header

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import NavTabs from "./NavTabs";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Resume from "./pages/Resume";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState("About");
@@ -27,9 +28,10 @@ export default function PortfolioContainer() {
   return (
     <div>
       {/* We are passing the currentPage from state and the function to update it */}
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
+      <Footer />
     </div>
   );
 }
